2df1ef30768b70bdbbc7473566eae718b439cc36
Jason <dljvaz@gmail.com>
2022-10-11 20:12:00 -0500

    first commit

---
+++ lib/pages/startup.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StartUp extends StatelessWidget {
+  const StartUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Default App"),)
+    );
+  }
+}
