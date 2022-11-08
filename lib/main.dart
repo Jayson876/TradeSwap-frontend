eb56c25c6d38d1a964b82a7e39117656f70221b8
Jason <dljvaz@gmail.com>
2022-11-08 08:32:23 -0500

    Changes to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: LandingScreen());
+    return const MaterialApp(home: LandingScreen());
   }
 }
