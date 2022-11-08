79cf28cce2a3ea19d7c44cf23621915066a1bc11
Jason <dljvaz@gmail.com>
2022-11-08 08:27:20 -0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'pages/startup.dart';
-import 'pages/client/client_signup.dart';
-import 'pages/tradesman/tradesman_signup.dart';
-import 'pages/signup.dart';
 
 
 void main() {
@@ -16,8 +13,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-
-    );
         debugShowCheckedModeBanner: false, home: LandingScreen());
   }
 }
